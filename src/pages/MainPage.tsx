0c8aba961716c42f039bc0decf41739e67d2bc0f
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-10 00:00:18 +0900

    feat: 현재 상영중인 영화 리스트 컴포넌트 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,11 @@
 import LatestMovieSection from "../features/movie/lastest";
+import NowPlayingSection from "../features/movie/nowPlaying";
 
 function MainPage() {
   return (
     <div>
       <LatestMovieSection />
+      <NowPlayingSection />
     </div>
   );
 }
