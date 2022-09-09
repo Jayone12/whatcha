b2949a551a90ed84ed3e60577e2935e4d8d0db8c
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-09 22:09:05 +0900

    fix: 환경변수 수정에 대해 일부 수정

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -6,9 +6,9 @@ import axios from "axios";
  * params: api key 및 언어 설정
  */
 const axiosInstance = axios.create({
-  baseURL: process.env.API_HOST,
+  baseURL: `${process.env.REACT_APP_API_HOST}/3`,
   params: {
-    api_key: process.env.API_KEY,
+    api_key: process.env.REACT_APP_API_KEY,
     language: "ko-KR",
   },
 });
