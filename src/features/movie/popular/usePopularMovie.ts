636750e3a9a551e4763b5a96cc47499e4e36b9de
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-11 01:55:47 +0900

    feat: 인기 영화 api 호출 커스텀 hook 생성

---
+++ src/features/movie/popular/usePopularMovie.ts
@@ -0,0 +1,17 @@
+import { AxiosError, AxiosResponse } from "axios";
+import { useQuery } from "react-query";
+import { popularApi } from "../../../apis/movieApi";
+import { ListResponse, MovieDetail } from "../../../types";
+
+/**
+ * 커스텀hook으로 react query를 사용하여 데이터 값을 리턴한다.
+ * @returns now playing Movie의 데이터를 리턴한다.
+ */
+function usePopularMovie() {
+  return useQuery<AxiosResponse<ListResponse<MovieDetail>>, AxiosError>(
+    "popularMovie",
+    popularApi
+  );
+}
+
+export default usePopularMovie;
