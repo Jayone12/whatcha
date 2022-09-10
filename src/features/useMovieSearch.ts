0fb438cad8ed68952b0a2ec055685050ea65ae19
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-11 04:58:13 +0900

    feat: 검색 결과에 대한 api 호출 커스텀 hook 생성

---
+++ src/features/useMovieSearch.ts
@@ -0,0 +1,15 @@
+import { AxiosError, AxiosResponse } from "axios";
+import { useQuery } from "react-query";
+import { searchApi } from "../apis/movieApi";
+import { ListResponse, MovieDetail } from "../types";
+
+const useMovieSearch = (query: string) => {
+  return useQuery<AxiosResponse<ListResponse<MovieDetail>>, AxiosError>(
+    ["searchMovie", query],
+    () => searchApi(query),
+    // 검색하려는 키워드 즉 query가 있어야 해당 커스텀 hook이 동작한다.
+    { enabled: Boolean(query) }
+  );
+};
+
+export default useMovieSearch;
