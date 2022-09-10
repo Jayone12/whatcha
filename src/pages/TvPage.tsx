8efb8952226471ddb11d820f3bf6eb471a3eb456
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-11 04:04:56 +0900

    feat: tv 시리즈에 대한 컴포넌트 추가

--- src/pages/TvPage.tsx
+++ src/pages/TvPage.tsx
@@ -1,5 +1,32 @@
+import styled from "@emotion/styled";
+import AiringTodayTvSection from "../features/tv/airingToday";
+import LatestTvSection from "../features/tv/latest";
+import OnTheAirSection from "../features/tv/onTheAir";
+import PopularSection from "../features/tv/popular";
+import TopRateSection from "../features/tv/topRate";
+
+const Main = styled.main`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const Container = styled.div`
+  margin-top: 62px;
+  padding: 24px 0;
+`;
+
 function TvPage() {
-  return <div></div>;
+  return (
+    <Main>
+      <Container>
+        <LatestTvSection />
+        <AiringTodayTvSection />
+        <OnTheAirSection />
+        <PopularSection />
+        <TopRateSection />
+      </Container>
+    </Main>
+  );
 }
 
 export default TvPage;
