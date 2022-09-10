1982b34cc044b206ad03a992d665e7fa4fb5ee74
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-11 01:56:40 +0900

    feat: 인기 영화 리스트 영역 컴포넌트 생성

---
+++ src/features/movie/popular/index.tsx
@@ -0,0 +1,45 @@
+import styled from "@emotion/styled";
+import Card from "../../../components/Card";
+import Slider from "../../../components/Slider";
+import usePopularMovie from "./usePopularMovie";
+
+const Base = styled.div`
+  margin-bottom: 42px;
+`;
+
+const Title = styled.h4`
+  font-size: 22px;
+  font-weight: 700;
+  line-height: 30px;
+  padding: 12px 0 14px;
+`;
+
+function PopulstSection() {
+  // 커스텀hook으로 data 불러오기
+  const { data, isLoading } = usePopularMovie();
+
+  const getYear = (date: string) => date.split("-")[0];
+  return (
+    <Base>
+      <Title>인기 상영작</Title>
+      {isLoading || !data ? (
+        <div>Loading...</div>
+      ) : (
+        <Slider>
+          {data.data.results.map((movie) => (
+            <Card
+              key={movie.id}
+              linkUrl={`/movie/${movie.id}`}
+              title={movie.title}
+              posterPath={`${process.env.REACT_APP_IMAGE_PREFIX}/${movie.poster_path}`}
+              voteAverage={movie.vote_average}
+              year={getYear(movie.release_date)}
+            />
+          ))}
+        </Slider>
+      )}
+    </Base>
+  );
+}
+
+export default PopulstSection;
