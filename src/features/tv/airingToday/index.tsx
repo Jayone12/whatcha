e882e5e01dca353f2afe30366310d9df6d91a5d3
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-11 03:46:07 +0900

    feat: 오늘 방송 영역 컴포넌트 생성

---
+++ src/features/tv/airingToday/index.tsx
@@ -0,0 +1,43 @@
+import styled from "@emotion/styled";
+import Card from "../../../components/Card";
+import Slider from "../../../components/Slider";
+import useAiringTodayTv from "./useAiringTodayTv";
+
+const Base = styled.div`
+  margin-bottom: 62px;
+`;
+
+const Title = styled.h4`
+  font-size: 22px;
+  font-weight: 700;
+  line-height: 30px;
+  padding: 12px 0 14px;
+`;
+
+function AiringTodayTvSection() {
+  const { data, isLoading } = useAiringTodayTv();
+
+  const getYear = (date: string) => date.split("-")[0];
+  return (
+    <Base>
+      <Title>Airing Today</Title>
+      {isLoading || !data ? (
+        <div>Loading...</div>
+      ) : (
+        <Slider>
+          {data.data.results.map((tv) => (
+            <Card
+              key={tv.id}
+              linkUrl={`/tv/${tv.id}`}
+              title={tv.name}
+              posterPath={`${process.env.REACT_APP_IMAGE_PREFIX}/${tv.poster_path}`}
+              voteAverage={tv.vote_average}
+              year={getYear(tv.first_air_date)}
+            />
+          ))}
+        </Slider>
+      )}
+    </Base>
+  );
+}
+export default AiringTodayTvSection;
