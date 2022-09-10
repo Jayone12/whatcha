b07bd78ad3df9f395883bcff430f886abcdf6176
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-11 02:09:11 +0900

    feat: 영화 카테코리별 컴포넌트 생성

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,17 @@
 import LatestMovieSection from "../features/movie/lastest";
 import NowPlayingSection from "../features/movie/nowPlaying";
+import PopularSection from "../features/movie/popular";
+import TopRateSection from "../features/movie/topRate";
+import UpcomingSection from "../features/movie/upcoming";
 
 function MainPage() {
   return (
     <div>
       <LatestMovieSection />
       <NowPlayingSection />
+      <PopularSection />
+      <TopRateSection />
+      <UpcomingSection />
     </div>
   );
 }
