68ddfe7f9a2706f5676791ce1b074b3ad8398b5a
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-11 03:52:16 +0900

    style: style 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,18 +1,31 @@
+import styled from "@emotion/styled";
 import LatestMovieSection from "../features/movie/lastest";
 import NowPlayingSection from "../features/movie/nowPlaying";
 import PopularSection from "../features/movie/popular";
 import TopRateSection from "../features/movie/topRate";
 import UpcomingSection from "../features/movie/upcoming";
 
+const Main = styled.main`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const Container = styled.div`
+  margin-top: 62px;
+  padding: 24px 0;
+`;
+
 function MainPage() {
   return (
-    <div>
-      <LatestMovieSection />
-      <NowPlayingSection />
-      <PopularSection />
-      <TopRateSection />
-      <UpcomingSection />
-    </div>
+    <Main>
+      <Container>
+        <LatestMovieSection />
+        <NowPlayingSection />
+        <PopularSection />
+        <TopRateSection />
+        <UpcomingSection />
+      </Container>
+    </Main>
   );
 }
 
